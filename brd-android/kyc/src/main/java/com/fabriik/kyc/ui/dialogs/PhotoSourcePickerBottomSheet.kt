0c9cc80a833d07de0ec4c2396bae2cee5c3193ca
Rok Novak <rok@equaleyes.com>
2022-05-12 12:23:18 +0200

    fix: add ripple effect to bottom_sheet_photo_source_picker views

--- brd-android/kyc/src/main/java/com/fabriik/kyc/ui/dialogs/PhotoSourcePickerBottomSheet.kt
+++ brd-android/kyc/src/main/java/com/fabriik/kyc/ui/dialogs/PhotoSourcePickerBottomSheet.kt
@@ -29,7 +29,7 @@ class PhotoSourcePickerBottomSheet : BottomSheetDialogFragment() {
         )
 
         with(binding) {
-            ivDismiss.setOnClickListener {
+            btnDismiss.setOnClickListener {
                 dismissAllowingStateLoss()
             }
 
