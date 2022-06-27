66b30a5a616152e729e13123c71459e32917d2bb
Lenart <l.rupnik@nchain.com>
2022-06-27 14:38:54 +0200

    Test: Extensions converters from pixels to dp/sp

--- brd-android/fabriik-common/src/test/java/com/fabriik/common/utils/Extensions.kt
+++ brd-android/fabriik-common/src/test/java/com/fabriik/common/utils/Extensions.kt
@@ -5,14 +5,21 @@ import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 class ExtensionsTest {
 
+    companion object {
+        private const val emptyString = ""
+        private const val text = "Test"
+        private const val intValue = 123
+    }
+
     private val editableNull = null
     private val editableEmpty = Editable.Factory().newEditable(emptyString)
     private val editableText = Editable.Factory().newEditable(text)
-    private val editableInt = Editable.Factory().newEditable(numbers.toString())
+    private val editableInt = Editable.Factory().newEditable(intValue.toString())
 
     @Test
     fun textOrEmpty_null_returnEmptyString() {
@@ -47,7 +54,7 @@ class ExtensionsTest {
     @Test
     fun asInt_editableInt_returnInt() {
         val actual = editableInt.asInt()
-        Assert.assertEquals(numbers, actual)
+        Assert.assertEquals(intValue, actual)
     }
 
     @Test(expected = NumberFormatException::class)
@@ -56,9 +63,91 @@ class ExtensionsTest {
         Assert.fail()
     }
 
-    companion object {
-        private const val emptyString = ""
-        private const val text = "Test"
-        private const val numbers = 123
+    @Test
+    @Config(qualifiers = "ldpi")
+    fun dp_ldpi_returnDP() {
+        val actual = intValue.dp
+        val expected = (intValue * 0.75).toInt()
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    @Config(qualifiers = "mdpi")
+    fun dp_mdpi_returnDP() {
+        val actual = intValue.dp
+        Assert.assertEquals(intValue, actual)
+    }
+
+    @Test
+    @Config(qualifiers = "hdpi")
+    fun dp_hdpi_returnDP() {
+        val actual = intValue.dp
+        val expected = (intValue * 1.5).toInt()
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    @Config(qualifiers = "xhdpi")
+    fun dp_xhdpi_returnDP() {
+        val actual = intValue.dp
+        Assert.assertEquals((intValue * 2), actual)
+    }
+
+    @Test
+    @Config(qualifiers = "xxhdpi")
+    fun dp_xxhdpi_returnDP() {
+        val actual = intValue.dp
+        Assert.assertEquals(intValue * 3, actual)
+    }
+
+    @Test
+    @Config(qualifiers = "xxxhdpi")
+    fun dp_xxxdpi_returnDP() {
+        val actual = intValue.dp
+        Assert.assertEquals(intValue * 4, actual)
+    }
+
+    @Test
+    @Config(qualifiers = "ldpi")
+    fun sp_ldpi_returnSP() {
+        val actual = intValue.sp
+        val expected = (intValue * 0.75).toInt()
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    @Config(qualifiers = "mdpi")
+    fun sp_mdpi_returnSP() {
+        val actual = intValue.sp
+        Assert.assertEquals(intValue, actual)
+    }
+
+    @Test
+    @Config(qualifiers = "hdpi")
+    fun sp_hdpi_returnSP() {
+        val actual = intValue.sp
+        val expected = (intValue * 1.5).toInt()
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    @Config(qualifiers = "xhdpi")
+    fun sp_xhdpi_returnSP() {
+        val actual = intValue.sp
+        Assert.assertEquals(intValue * 2, actual)
+    }
+
+    @Test
+    @Config(qualifiers = "xxhdpi")
+    fun sp_xxhdpi_returnSP() {
+        val actual = intValue.sp
+        Assert.assertEquals(intValue * 3, actual)
+    }
+
+    @Test
+    @Config(qualifiers = "xxxhdpi")
+    fun sp_xxxhdpi_returnSP() {
+        val actual = intValue.sp
+        Assert.assertEquals(intValue * 4, actual)
     }
 }
