000dde669bbc4fa30a44d2616a913d44845242a1
Rok Novak <rok@equaleyes.com>
2022-06-30 06:54:36 +0200

    feat: implement FabriikSwitch

--- brd-android/fabriik-common/src/main/java/com/fabriik/common/ui/customview/FabriikSwitch.kt
+++ brd-android/fabriik-common/src/main/java/com/fabriik/common/ui/customview/FabriikSwitch.kt
@@ -1,13 +1,11 @@
 package com.fabriik.common.ui.customview
 
 import android.content.Context
-import android.graphics.Color
 import android.util.AttributeSet
 import android.view.ContextThemeWrapper
-import android.view.Gravity
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.content.ContextCompat
+import androidx.annotation.IntDef
 import androidx.core.view.setPadding
 import com.fabriik.common.R
 import com.fabriik.common.utils.dp
@@ -26,14 +24,37 @@ class FabriikSwitch @JvmOverloads constructor(
         setPadding(4.dp)
         setBackgroundResource(R.drawable.bg_rounded_tertiary)
 
-        tvLeftOption = addOptionView("Option 1")
-        tvRightOption = addOptionView("Option 2")
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.FabriikSwitch)
+        val leftOptionText = typedArray.getString(R.styleable.FabriikSwitch_leftOptionText) ?: ""
+        val rightOptionText = typedArray.getString(R.styleable.FabriikSwitch_rightOptionText) ?: ""
+        val selectedOption = typedArray.getInt(R.styleable.FabriikSwitch_selectedOption, 0)
+        typedArray.recycle()
+
+        tvLeftOption = addOptionView(leftOptionText)
+        tvRightOption = addOptionView(rightOptionText)
+
+        setSelectedItem(selectedOption)
     }
 
-    private fun addOptionView(text: String) : TextView {
+    private fun setSelectedItem(@SwitchOption selectedOption: Int) {
+        tvLeftOption.isActivated = selectedOption == OPTION_LEFT
+        tvRightOption.isActivated = selectedOption == OPTION_RIGHT
+    }
+
+    private fun addOptionView(text: CharSequence) : TextView {
         val tvOption = TextView(ContextThemeWrapper(context, R.style.FabriikCustomSwitchItemStyle))
         tvOption.text = text
         addView(tvOption, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 0.5f))
         return tvOption
+    }
+
+    companion object {
+        @IntDef(OPTION_NONE, OPTION_LEFT, OPTION_RIGHT)
+        @Retention(AnnotationRetention.SOURCE)
+        annotation class SwitchOption
+
+        const val OPTION_NONE = 0
+        const val OPTION_LEFT = 1
+        const val OPTION_RIGHT = 2
     }
 }
