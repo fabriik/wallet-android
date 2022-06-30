3570d4a1b352da106f15bb5ebcef45fa7cef0427
dabitdev <david@nordicloop.com>
2022-06-30 16:31:21 +0200

    adding navigation

--- brd-android/trade/src/main/java/com/fabriik/trade/ui/features/swap/SwapInputFragment.kt
+++ brd-android/trade/src/main/java/com/fabriik/trade/ui/features/swap/SwapInputFragment.kt
@@ -9,6 +9,7 @@ import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.breadwallet.tools.manager.BRSharedPrefs
 import com.fabriik.common.ui.base.FabriikView
 import com.fabriik.trade.R
@@ -38,6 +39,11 @@ class SwapInputFragment : Fragment(),
         with(binding) {
             toolbar.setDismissButtonClickListener {
                 viewModel.setEvent(SwapInputContract.Event.DismissClicked)
+            }
+
+            btnConfirm.setOnClickListener {
+                // TODO add here the current pair
+                findNavController().navigate(SwapInputFragmentDirections.actionFragmentSwapInputToFragmentSwapProcessing("ETH", "BTC"))
             }
 
             //todo: for test only
