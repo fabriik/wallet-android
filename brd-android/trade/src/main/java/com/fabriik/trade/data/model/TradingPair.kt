1e752e4a6801f2fba6d11e4796c6c07fbd7eb207
Rok <rok@equaleyes.com>
2022-07-07 13:51:15 +0200

    feat: add SwapApi (#146)

---
+++ brd-android/trade/src/main/java/com/fabriik/trade/data/model/TradingPair.kt
@@ -0,0 +1,27 @@
+package com.fabriik.trade.data.model
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
+import java.math.BigDecimal
+
+@Parcelize
+@JsonClass(generateAdapter = true)
+data class TradingPair(
+
+    @Json(name = "base_currency")
+    val baseCurrency: String,
+
+    @Json(name = "term_currency")
+    val termCurrency: String,
+
+    @Json(name = "minimum_quantity")
+    val minAmount: BigDecimal,
+
+    @Json(name = "maximum_quantity")
+    val maxAmount: BigDecimal,
+
+    @Json(name = "name")
+    val name: String
+): Parcelable
