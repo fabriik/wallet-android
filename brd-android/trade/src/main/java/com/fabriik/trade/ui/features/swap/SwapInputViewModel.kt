17d49c528819f36ade01d6fbf737b55bf761a1a5
Rok <rok@equaleyes.com>
2022-07-13 11:40:09 +0200

    fix: convert fiat to crpyto for maxAmount limit (#168)

--- brd-android/trade/src/main/java/com/fabriik/trade/ui/features/swap/SwapInputViewModel.kt
+++ brd-android/trade/src/main/java/com/fabriik/trade/ui/features/swap/SwapInputViewModel.kt
@@ -48,7 +48,7 @@ class SwapInputViewModel(
     private val currentLoadedState: SwapInputContract.State.Loaded?
         get() = state.value as SwapInputContract.State.Loaded?
 
-    private val maxAmountLimit = profileManager.getProfile().nextExchangeLimit()
+    private val maxAmountLimitFiat = profileManager.getProfile().nextExchangeLimit()
     private val ratesRepository by kodein.instance<RatesRepository>()
 
     private var currentTimerJob: Job? = null
@@ -175,8 +175,13 @@ class SwapInputViewModel(
     private fun onMaxAmountClicked() {
         val state = currentLoadedState ?: return
 
+        val maxAmountLimitCrypto = maxAmountLimitFiat.toCrypto(
+            cryptoCurrency = state.sourceCryptoCurrency,
+            fiatCurrency = state.fiatCurrency
+        )
+
         onSourceCurrencyCryptoAmountChanged(
-            min(state.sourceCryptoBalance, maxAmountLimit), false
+            min(state.sourceCryptoBalance, maxAmountLimitCrypto), false
         )
     }
 
@@ -308,7 +313,7 @@ class SwapInputViewModel(
                 SwapInputContract.State.Loaded(
                     tradingPairs = tradingPairs,
                     selectedPair = selectedPair,
-                    fiatCurrency = BRSharedPrefs.getPreferredFiatIso(),
+                    fiatCurrency = "USD",
                     quoteResponse = selectedPairQuote,
                     sourceCryptoCurrency = selectedPair.baseCurrency,
                     destinationCryptoCurrency = selectedPair.termCurrency,
