79c79690484392e090dd78921599860f861c8db5
Rok Novak <rok@equaleyes.com>
2022-07-25 11:25:46 +0200

    fix: remove unused class

--- brd-android/app/src/main/java/com/breadwallet/ui/wallet/WalletScreen.kt
+++ brd-android/app/src/main/java/com/breadwallet/ui/wallet/WalletScreen.kt
@@ -221,7 +221,6 @@ object WalletScreen {
         data class LoadSyncState(val currencyCode: String) : F()
         data class LoadWalletBalance(val currencyCode: String) : F()
         data class LoadTransactions(val currencyCode: String) : F()
-        data class LoadSwapTransactions(val currencyCode: String) : F()
         data class LoadFiatPricePerUnit(val currencyCode: String) : F()
         data class LoadTransactionMetaData(
             val currencyCode: String,
