024af8c74590ca0458f819cae7316ab8fbe5d3ed
Lenart <l.rupnik@nchain.com>
2022-07-28 13:28:13 +0200

    feat: Credit card custom view

---
+++ brd-android/buy/src/main/java/com/fabriik/buy/ui/customview/CreditCardView.kt
@@ -0,0 +1,13 @@
+package com.fabriik.buy.ui.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.card.MaterialCardView
+
+class CreditCardView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+
+    ) : MaterialCardView(context, attrs) {
+
+        private val binding:
+}
