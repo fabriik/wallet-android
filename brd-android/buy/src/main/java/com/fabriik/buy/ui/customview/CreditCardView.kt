7e7d526303e33ef2e33054db0afb7a2a61bf1267
Lenart <l.rupnik@nchain.com>
2022-07-28 15:52:07 +0200

    feat: Credit card custom view

--- brd-android/buy/src/main/java/com/fabriik/buy/ui/customview/CreditCardView.kt
+++ brd-android/buy/src/main/java/com/fabriik/buy/ui/customview/CreditCardView.kt
@@ -2,12 +2,31 @@ package com.fabriik.buy.ui.customview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.core.content.ContextCompat
+import com.fabriik.buy.R
+import com.fabriik.buy.databinding.ViewCreditCardBinding
+import com.fabriik.common.utils.dp
 import com.google.android.material.card.MaterialCardView
 
 class CreditCardView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 
-    ) : MaterialCardView(context, attrs) {
+) : MaterialCardView(context, attrs) {
 
-        private val binding:
+    private val binding: ViewCreditCardBinding
+
+    init {
+        radius = 16.dp.toFloat()
+        binding = ViewCreditCardBinding.inflate(LayoutInflater.from(context), this, true)
+        backgroundTintList = ContextCompat.getColorStateList(context, R.color.fabriik_dark_blue_buy)
+    }
+
+    fun setLastDigits(digits: String) {
+        binding.tvLastDigits.text = digits
+    }
+
+    fun setExpirationDate(date: String) {
+        binding.tvLastDigits.text = date
+    }
 }
