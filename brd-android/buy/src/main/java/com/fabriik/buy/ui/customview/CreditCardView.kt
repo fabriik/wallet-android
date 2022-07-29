f6773c5292341797f5cd3174d4b633f90926264c
Lenart Rupnik <105631640+lrupnikk@users.noreply.github.com>
2022-07-29 16:13:01 +0200

    feat: CreditCard customview (#230)
    * feat: Credit card custom view
    
    * feat: Credit card custom view
    
    * feat: add country selection to Billing address screen (#211)
    
    * Re-add unmerged changes (#212)
    
    * feat: add Check assets and Temp unavailable dialogs
    
    * feat: add NoInternet activity
    
    * feat: show no internet activity
    
    * fix: update error when quote fails
    
    * fix: update error when quote fails
    
    * feat: handle quote expired error
    
    * feat: show KYC2 exchange limit error
    
    * feat: check eth balance after fees are calculated
    
    * feat: show eth balance warning and error for fees
    
    * Refactoring - AmountConverter (#210)
    
    * refactor: replace .toFiat with amountConverter
    
    * refactor: replace .toCrypto with amountConverter
    
    * feat: move code to AmountConverter
    
    * feat: remove min and max quantity fields (#215)
    
    * Buy screen - UI (#213)
    
    * feat: add Check assets and Temp unavailable dialogs
    
    * feat: add NoInternet activity
    
    * feat: show no internet activity
    
    * fix: update error when quote fails
    
    * fix: update error when quote fails
    
    * feat: handle quote expired error
    
    * feat: show KYC2 exchange limit error
    
    * feat: check eth balance after fees are calculated
    
    * feat: show eth balance warning and error for fees
    
    * Refactoring - AmountConverter (#210)
    
    * refactor: replace .toFiat with amountConverter
    
    * refactor: replace .toCrypto with amountConverter
    
    * feat: move code to AmountConverter
    
    * feat: add country selection to Billing address screen
    
    * feat: add events to BuyInputFragment
    
    * feat: convert amounts
    
    * feat: update calculations
    
    * fix: resolve Click on About does not open About screen (#218)
    
    * fix: resolve Click on Feedback does not open email client (#219)
    
    * fix: resolve Transaction history cannot be exported as CSV issue (#220)
    
    * Re-add unmerged changes (#221)
    
    * feat: add Check assets and Temp unavailable dialogs
    
    * feat: add NoInternet activity
    
    * feat: show no internet activity
    
    * fix: update error when quote fails
    
    * fix: update error when quote fails
    
    * feat: handle quote expired error
    
    * feat: show KYC2 exchange limit error
    
    * feat: check eth balance after fees are calculated
    
    * feat: show eth balance warning and error for fees
    
    * Refactoring - AmountConverter (#210)
    
    * refactor: replace .toFiat with amountConverter
    
    * refactor: replace .toCrypto with amountConverter
    
    * feat: move code to AmountConverter
    
    * Swap amount input fixes (#214)
    
    * feat: show keyboard on first click
    
    * fix: prevent fee estimation if amount is zero
    
    * fix: prevent fee estimation if amount is zero
    
    * fix: prevent deletion when user start amount with zero
    
    * Update /quote endpoint response  (#217)
    
    * fix: update quote response
    
    * feat: add markup to calculations
    
    * feat: update contract
    
    * feat: add markup to calculations
    
    * fix: resolve calculation issues
    
    * feat: Basic Buy orderPreview screen (#216)
    
    * feat: Prepare buy OrderPreview screen
    
    * refactor: InfoDialog location & buy map order
    
    * feat: Show info dialog on OrderPreview screen
    
    * refactor: Move AddCard screen
    
    * releasing 4.13.1.66
    
    * releasing 4.13.1.67
    
    * fix: resolve nav_graph issues (CountrySelectionArgs duplicated) (#222)
    
    * releasing 4.13.1.68
    
    * Resolve nav graph issues (AssetSelectionArgs duplicated) (#223)
    
    * fix: resolve nav_graph issues (CountrySelectionArgs duplicated)
    
    * fix: resolve nav_graph issues (AssetSelectionArgs duplicated)
    
    * fix: resolve nav_graph issues (AssetSelectionArgs duplicated)
    
    * releasing 4.13.1.69
    
    * feat: Credit card custom view
    
    * feat: Credit card custom view
    
    * refactor: Improve code by comments
    
    Co-authored-by: Rok <rok@equaleyes.com>
    Co-authored-by: dabitdev <david@nordicloop.com>

---
+++ brd-android/buy/src/main/java/com/fabriik/buy/ui/customview/CreditCardView.kt
@@ -0,0 +1,49 @@
+package com.fabriik.buy.ui.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
+import com.fabriik.buy.R
+import com.fabriik.buy.databinding.ViewCreditCardBinding
+import com.fabriik.common.utils.dp
+import com.google.android.material.card.MaterialCardView
+
+class CreditCardView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : MaterialCardView(context, attrs) {
+
+    private val binding: ViewCreditCardBinding
+
+    init {
+        binding = ViewCreditCardBinding.inflate(LayoutInflater.from(context), this, true)
+        radius = 8.dp.toFloat()
+        setContentPadding(12.dp,7.dp,8.dp,7.dp)
+        backgroundTintList = ContextCompat.getColorStateList(context, R.color.fabriik_dark_blue_buy)
+        elevation = 0.dp.toFloat()
+    }
+
+    fun setContent(type: CardType, lastDigits: String, expirationDate: String) {
+        setIcon(type.icon)
+        setLastDigits(lastDigits)
+        setExpirationDate(expirationDate)
+    }
+
+    private fun setIcon(icon: Int) {
+        binding.icCardLogo.setImageDrawable(AppCompatResources.getDrawable(context, icon))
+    }
+
+    private fun setLastDigits(digits: String) {
+        binding.tvLastDigits.text = digits
+    }
+
+    private fun setExpirationDate(date: String) {
+        binding.tvDate.text = date
+    }
+}
+
+enum class CardType(@DrawableRes val icon: Int) {
+    VISA(R.drawable.ic_visa)
+}
