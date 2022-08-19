497415ff0adf28a6a01d16b7d0b2de574b7d6a84
Rok Novak <rok@equaleyes.com>
2022-08-19 13:20:48 +0200

    fix: update profile model

--- brd-android/fabriik-common/src/main/java/com/fabriik/common/data/model/ExchangeLimits.kt
+++ brd-android/fabriik-common/src/main/java/com/fabriik/common/data/model/ExchangeLimits.kt
@@ -11,26 +11,18 @@ import java.util.*
 @Parcelize
 @JsonClass(generateAdapter = false)
 data class ExchangeLimits(
-    @Json(name = "allowance_lifetime")
-    val allowanceLifetime: BigDecimal,
+    @Json(name = "swap_allowance_lifetime")
+    val swapAllowanceLifetime: BigDecimal?,
 
-    @Json(name = "allowance_daily")
-    val allowanceDaily: BigDecimal,
+    @Json(name = "swap_allowance_daily")
+    val swapAllowanceDaily: BigDecimal?,
 
-    @Json(name = "allowance_per_exchange")
-    val allowancePerExchange: BigDecimal,
+    @Json(name = "swap_allowance_per_exchange")
+    val swapAllowancePerExchange: BigDecimal?,
 
-    @Json(name = "used_lifetime")
-    val usedLifetime: BigDecimal,
+    @Json(name = "used_swap_lifetime")
+    val swapUsedLifetime: BigDecimal?,
 
-    @Json(name = "used_daily")
-    val usedDaily: BigDecimal,
-
-    @Json(name = "next_exchange_limit")
-    val nextExchangeLimit: BigDecimal
-) : Parcelable {
-
-    fun availableDaily() = allowanceDaily - usedDaily
-
-    fun availableLifetime() = allowanceLifetime - usedLifetime
-}
+    @Json(name = "used_swap_daily")
+    val swapUsedDaily: BigDecimal?,
+) : Parcelable
