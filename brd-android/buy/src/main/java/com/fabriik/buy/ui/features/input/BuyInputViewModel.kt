c01f8fc9395e05266eece64cb3be2940e310dacb
Rok <rok@equaleyes.com>
2022-08-25 09:02:23 +0200

    fix: preselect payment card on Buy screen (#276)

--- brd-android/buy/src/main/java/com/fabriik/buy/ui/features/input/BuyInputViewModel.kt
+++ brd-android/buy/src/main/java/com/fabriik/buy/ui/features/input/BuyInputViewModel.kt
@@ -197,6 +197,7 @@ class BuyInputViewModel(
                     cryptoCurrency = firstWallet,
                     supportedCurrencies = supportedCurrencies,
                     paymentInstruments = instrumentsResponse.data ?: emptyList(),
+                    selectedPaymentMethod = instrumentsResponse.data?.lastOrNull(),
                     profile = profileManager.getProfile()
                 )
             }
