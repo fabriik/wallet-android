0d10b9a1cef4b2307e9503aac892e9d74fbafc46
UrbanFercec <79310380+UrbanFercec@users.noreply.github.com>
2022-08-29 11:34:21 +0200

    fix: home and trade access based on verified statuses

--- brd-android/fabriik-common/src/main/java/com/fabriik/common/data/model/Profile.kt
+++ brd-android/fabriik-common/src/main/java/com/fabriik/common/data/model/Profile.kt
@@ -37,16 +37,17 @@ data class Profile(
     val exchangeLimits: ExchangeLimits?,
 
     @Json(name = "roles")
-    val roles: List<ProfileRole?>?
+    val roles: List<ProfileRole?>?,
 ) : Parcelable
 
 fun Profile?.isRegistrationNeeded() = this == null || noRole(ProfileRole.CUSTOMER)
 
 fun Profile?.isEmailVerificationNeeded() = allRoles(ProfileRole.CUSTOMER, ProfileRole.UNVERIFIED)
 
-fun Profile?.canUseBuyTrade() = hasRole(ProfileRole.CUSTOMER) &&
-        anyRole(ProfileRole.KYC_LEVEL_1, ProfileRole.KYC_LEVEL_2) &&
-        noRole(ProfileRole.UNVERIFIED)
+fun Profile?.canUseBuy() = isVerifiedCustomer() && hasRole(ProfileRole.KYC_LEVEL_2)
+
+fun Profile?.canUseTrade() =
+    isVerifiedCustomer() && anyRole(ProfileRole.KYC_LEVEL_1, ProfileRole.KYC_LEVEL_2)
 
 private fun Profile?.noRole(role: ProfileRole) = !hasRole(role)
 
@@ -56,6 +57,9 @@ private fun Profile?.anyRole(vararg roles: ProfileRole) = roles.any { hasRole(it
 
 private fun Profile?.allRoles(vararg roles: ProfileRole) = roles.all { hasRole(it) }
 
+private fun Profile?.isVerifiedCustomer() =
+    hasRole(ProfileRole.CUSTOMER) && noRole(ProfileRole.UNVERIFIED)
+
 fun Profile.isKyc1(): Boolean = hasRole(ProfileRole.KYC_LEVEL_1)
 
-fun Profile.isKyc2(): Boolean  = hasRole(ProfileRole.KYC_LEVEL_2)
+fun Profile.isKyc2(): Boolean = hasRole(ProfileRole.KYC_LEVEL_2)
