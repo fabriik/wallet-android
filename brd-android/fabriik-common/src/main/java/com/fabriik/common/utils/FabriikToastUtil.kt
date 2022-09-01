f45db5629ae6310d9d22c32dbbc7ea47d4821a17
UrbanFercec <79310380+UrbanFercec@users.noreply.github.com>
2022-09-01 08:40:58 +0200

    fix: added snackbar margin at the size of status bar

--- brd-android/fabriik-common/src/main/java/com/fabriik/common/utils/FabriikToastUtil.kt
+++ brd-android/fabriik-common/src/main/java/com/fabriik/common/utils/FabriikToastUtil.kt
@@ -1,5 +1,11 @@
 package com.fabriik.common.utils
 
+import android.app.Activity
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Rect
+import android.os.Build
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +13,7 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.setPadding
 import com.fabriik.common.R
 import com.google.android.material.snackbar.Snackbar
@@ -40,11 +47,13 @@ object FabriikToastUtil {
         )
     }
 
+    private const val BASE_STATUS_BAR_SIZE = 78
+
     private fun showCustomSnackBar(
         parentView: View,
         message: String,
         gravity: Int,
-        @DrawableRes background: Int
+        @DrawableRes background: Int,
     ) {
         val view = TextView(parentView.context).apply {
             text = message
@@ -55,16 +64,18 @@ object FabriikToastUtil {
         val snackBar = Snackbar.make(parentView, "", Snackbar.LENGTH_LONG).apply {
             this.view.setBackgroundResource(background)
         }
+        val height = getStatusBarHeight((parentView.context as Activity))
 
         // setup snackBar view
         (snackBar.view as Snackbar.SnackbarLayout).let {
             val params = it.layoutParams as ViewGroup.LayoutParams
             params.width = FrameLayout.LayoutParams.MATCH_PARENT
-
             if (params is CoordinatorLayout.LayoutParams) {
                 params.gravity = gravity
+                params.topMargin = height
             } else if (params is FrameLayout.LayoutParams) {
                 params.gravity = gravity
+                params.topMargin = height
             }
 
             it.layoutParams = params
@@ -72,5 +83,16 @@ object FabriikToastUtil {
         }
 
         snackBar.show()
+    }
+
+    private const val STATUS_BAR_NAME = "status_bar_height"
+    private const val STATUS_BAR_TYPE = "dimen"
+    private const val STATUS_BAR_PACKAGE = "android"
+
+    private fun getStatusBarHeight(activity: Activity): Int {
+        val resourceId =
+            activity.resources.getIdentifier(STATUS_BAR_NAME, STATUS_BAR_TYPE, STATUS_BAR_PACKAGE)
+        return if (resourceId > 0) activity.resources.getDimensionPixelSize(resourceId)
+        else Rect().apply { activity.window.decorView.getWindowVisibleDisplayFrame(this) }.top
     }
 }
