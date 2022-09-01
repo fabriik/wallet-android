02aecf1316ab5f7aea3b701d9ac77dd95f728f07
UrbanFercec <79310380+UrbanFercec@users.noreply.github.com>
2022-09-01 10:40:05 +0200

    fix: added snackbar margin at the size of status bar (#305)
    * fix: added snackbar margin at the size of status bar
    
    * fix: removed unused variable
    
    * fix: removed unused imports
    
    * fix: removed unneeded trailing comma

--- brd-android/fabriik-common/src/main/java/com/fabriik/common/utils/FabriikToastUtil.kt
+++ brd-android/fabriik-common/src/main/java/com/fabriik/common/utils/FabriikToastUtil.kt
@@ -1,5 +1,7 @@
 package com.fabriik.common.utils
 
+import android.app.Activity
+import android.graphics.Rect
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
@@ -55,16 +57,18 @@ object FabriikToastUtil {
         val snackBar = Snackbar.make(parentView, "", Snackbar.LENGTH_LONG).apply {
             this.view.setBackgroundResource(background)
         }
+        val height = getStatusBarHeight((parentView.context as Activity))
 
         // setup snackBar view
         (snackBar.view as Snackbar.SnackbarLayout).let {
             val params = it.layoutParams as ViewGroup.LayoutParams
             params.width = FrameLayout.LayoutParams.MATCH_PARENT
-
             if (params is CoordinatorLayout.LayoutParams) {
                 params.gravity = gravity
+                params.topMargin = height
             } else if (params is FrameLayout.LayoutParams) {
                 params.gravity = gravity
+                params.topMargin = height
             }
 
             it.layoutParams = params
@@ -72,5 +76,16 @@ object FabriikToastUtil {
         }
 
         snackBar.show()
+    }
+
+    private const val STATUS_BAR_NAME = "status_bar_height"
+    private const val STATUS_BAR_TYPE = "dimen"
+    private const val STATUS_BAR_PACKAGE = "android"
+
+    private fun getStatusBarHeight(activity: Activity): Int {
+        val resourceId =
+            activity.resources.getIdentifier(STATUS_BAR_NAME, STATUS_BAR_TYPE, STATUS_BAR_PACKAGE)
+        return if (resourceId > 0) activity.resources.getDimensionPixelSize(resourceId)
+        else Rect().apply { activity.window.decorView.getWindowVisibleDisplayFrame(this) }.top
     }
 }
