901f331ad6ead30602128b776937c4d96f5e3f9b
Rok Novak <rok@equaleyes.com>
2022-09-05 12:02:13 +0200

    tests: add AddCardEventHandlerTest in BillingAddressEventHandlerTest

--- brd-android/buy/src/test/java/com/fabriik/buy/ui/features/addcard/AddCardEventHandlerTest.kt
+++ brd-android/buy/src/test/java/com/fabriik/buy/ui/features/addcard/AddCardEventHandlerTest.kt
@@ -17,4 +17,49 @@ class AddCardEventHandlerTest {
         override fun onSecurityCodeChanged(securityCode: String) {}
         override fun onExpirationDateChanged(expirationDate: String) {}
     }
+
+    @Test
+    fun handleEvent_backClicked_callOnBackClicked() {
+        handler.handleEvent(AddCardContract.Event.BackClicked)
+        verify(handler).onBackClicked()
+    }
+
+    @Test
+    fun handleEvent_dismissClicked_callOnDismissClicked() {
+        handler.handleEvent(AddCardContract.Event.DismissClicked)
+        verify(handler).onDismissClicked()
+    }
+
+    @Test
+    fun handleEvent_confirmClicked_callOnConfirmClicked() {
+        handler.handleEvent(AddCardContract.Event.ConfirmClicked)
+        verify(handler).onConfirmClicked()
+    }
+
+    @Test
+    fun handleEvent_onDateChanged_callOnExpirationDateChanged() {
+        val date = "11/22"
+        handler.handleEvent(AddCardContract.Event.OnDateChanged(date))
+        verify(handler).onExpirationDateChanged(date)
+    }
+
+    @Test
+    fun handleEvent_onCardNumberChanged_callOnCardNumberChanged() {
+        val cardNumber = "1234 1234 1234 1234"
+        handler.handleEvent(AddCardContract.Event.OnCardNumberChanged(cardNumber))
+        verify(handler).onCardNumberChanged(cardNumber)
+    }
+
+    @Test
+    fun handleEvent_onSecurityCodeChanged_callOnSecurityCodeChanged() {
+        val securityCode = "123"
+        handler.handleEvent(AddCardContract.Event.OnSecurityCodeChanged(securityCode))
+        verify(handler).onSecurityCodeChanged(securityCode)
+    }
+
+    @Test
+    fun handleEvent_securityCodeInfoClicked_callOnSecurityCodeInfoClicked() {
+        handler.handleEvent(AddCardContract.Event.SecurityCodeInfoClicked)
+        verify(handler).onSecurityCodeInfoClicked()
+    }
 }
