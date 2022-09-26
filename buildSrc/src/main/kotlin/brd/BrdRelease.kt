d6e74124ce02d8e436f3a22958de5fea9ac0e645
dabitdev <david@nordicloop.com>
2022-09-26 08:05:29 -0300

    preparing 4.14.0.15 reverting target sdk

--- buildSrc/src/main/kotlin/brd/BrdRelease.kt
+++ buildSrc/src/main/kotlin/brd/BrdRelease.kt
@@ -41,7 +41,7 @@ object BrdRelease {
     private val engineering = ciTag?.get(2) ?: 0
 
     /** Build version. Increase for each new build. Maximum value: 999 */
-    private val build = ciTag?.lastOrNull() ?: 14
+    private val build = ciTag?.lastOrNull() ?: 15
 
     init {
         check(marketing in 0..99)
@@ -57,7 +57,7 @@ object BrdRelease {
     val buildVersion = build
     val internalVersionName = "$marketing.$product.$engineering.$build"
 
-    const val ANDROID_TARGET_SDK = 31
+    const val ANDROID_TARGET_SDK = 30
     const val ANDROID_COMPILE_SDK = 31
     const val ANDROID_MINIMUM_SDK = 24
     const val ANDROID_BUILD_TOOLS = "30.0.2"
